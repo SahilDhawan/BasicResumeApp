cca793bf850c69783402523656bb3555122206da
Sahil Dhawan <sahildhawan2012@icloud.com>
2016-10-24 00:10:32 +0530

    Initial Commit

---
+++ basicAnimations-1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  basicAnimations-1
+//
+//  Created by Sahil Dhawan on 24/10/16.
+//  Copyright © 2016 Sahil Dhawan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ basicAnimations-1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  basicAnimations-1
+//
+//  Created by Sahil Dhawan on 24/10/16.
+//  Copyright © 2016 Sahil Dhawan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
