e517f99ede9dcc4e55a44222f45e353c2190f759
Sahil Dhawan <sahildhawan2012@icloud.com>
2016-10-30 01:50:49 +0530

    first commit

---
+++ basicAnimations-1/LaunchScreenViewController.h
@@ -0,0 +1,13 @@
+//
+//  LaunchScreenViewController.h
+//  basicAnimations-1
+//
+//  Created by Sahil Dhawan on 24/10/16.
+//  Copyright © 2016 Sahil Dhawan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LaunchScreenViewController : UIViewController
+
+@end
---
+++ basicAnimations-1/LaunchScreenViewController.m
@@ -0,0 +1,37 @@
+//
+//  LaunchScreenViewController.m
+//  basicAnimations-1
+//
+//  Created by Sahil Dhawan on 24/10/16.
+//  Copyright © 2016 Sahil Dhawan. All rights reserved.
+//
+
+#import "LaunchScreenViewController.h"
+
+@interface LaunchScreenViewController ()
+
+@end
+
+@implementation LaunchScreenViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
